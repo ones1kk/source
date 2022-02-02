196985be6bf1616a5d9fbdc6a86e86ef5d8a9dd0
ones1kk <kwsic@naver.com>
2022-02-02 23:40:01 +0900

    Add : Sample label structure

---
+++ src/main/java/com/sample/source/property/LabelService.java
@@ -0,0 +1,17 @@
+package com.sample.source.property;
+
+import com.sample.source.property.repository.LabelMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LabelService {
+
+    private final LabelMapper mapper;
+
+    public Object findAllLabels() {
+        return mapper.findAllLabels();
+    }
+
+}
