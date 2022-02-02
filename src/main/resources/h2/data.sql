1eae33213850864d18767e8003d2249eb90b9c0b
ones1kk <kwsic@naver.com>
2022-02-02 23:40:30 +0900

    🔧 Add : Init sample datas

---
+++ src/main/resources/h2/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO LABEL (LABEL_ID, LABEL_KR_NM, LABEL_ENG_NM)
+VALUES ('LABEL.01', '테스트1', 'TEST'),
+       ('LABEL.02', '테스트2', 'TEST'),
+       ('LABEL.03', '테스트3', 'TEST'),
+       ('LABEL.04', '테스트4', 'TEST'),
+       ('LABEL.05', '테스트5', 'TEST'),
+       ('LABEL.06', '테스트6', 'TEST'),
+       ('LABEL.07', '테스트7', 'TEST'),
+       ('LABEL.08', '테스트8', 'TEST'),
+       ('LABEL.09', '테스트9', 'TEST'),
+       ('LABEL.10', '테스트10', 'TEST'),
+       ('LABEL.11', '테스트11', 'TEST'),
+       ('LABEL.12', '테스트12', 'TEST');
