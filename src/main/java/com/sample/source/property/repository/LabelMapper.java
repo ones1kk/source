3f82ea28533d277b3518f250e700689e3f9f6595
ones1kk <kwsic@naver.com>
2022-03-20 22:10:44 +0900

    🔧 Update : LebelMapper

--- src/main/java/com/sample/source/property/repository/LabelMapper.java
+++ src/main/java/com/sample/source/property/repository/LabelMapper.java
@@ -1,12 +1,12 @@
 package com.sample.source.property.repository;
 
-import com.sample.source.property.Label;
-import java.util.List;
+import com.sample.source.property.label.Label;
+import java.util.Set;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface LabelMapper {
 
-    List<Label> findAllLabel();
+    Set<Label> findAllLabels();
 
 }
