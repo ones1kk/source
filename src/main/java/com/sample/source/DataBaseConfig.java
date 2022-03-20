ded2fc7aa61328ffd7555ed07f5b9e8f7b2c7d9d
ones1kk <kwsic@naver.com>
2022-03-20 22:09:38 +0900

    🔧 Update : DataBaseConfig

--- src/main/java/com/sample/source/DataBaseConfig.java
+++ src/main/java/com/sample/source/DataBaseConfig.java
@@ -1,20 +1,19 @@
 package com.sample.source;
 
-import com.sample.source.property.Label;
 import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-@Configuration
+@org.springframework.context.annotation.Configuration
 @MapperScan(annotationClass = Mapper.class, basePackageClasses = SourceApplication.class, sqlSessionFactoryRef = "sqlSessionFactory")
 public class DataBaseConfig {
 
@@ -29,14 +28,17 @@ public class DataBaseConfig {
     @Bean
     static SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+
+        Configuration configuration = new Configuration();
+        configuration.setMapUnderscoreToCamelCase(true);
+
         factoryBean.setDataSource(dataSource);
+        factoryBean.setConfiguration(configuration);
+
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         factoryBean.setMapperLocations(
             resolver.getResources("classpath:mybatis-mapper/*.xml"));
-        factoryBean.setTypeAliases(Label.class);
-        factoryBean.getObject()
-            .getConfiguration()
-            .setMapUnderscoreToCamelCase(true);
+
         return factoryBean.getObject();
     }
 
